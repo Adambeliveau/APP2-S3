437637f9d9b6031c9b25c98b88d0681fb2108ffe
AdamBeliveau <bela1003@usherbrooke.ca>
2020-05-30 11:13:21 -0400

    allo

--- eq11pro0/src/main/java/application/EMR.java
+++ eq11pro0/src/main/java/application/EMR.java
@@ -1,5 +1,50 @@
 package application;
 
-public class EMR {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class EMR extends Application {
+
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+	@Override
+	public void start(Stage primaryStage) {
+		
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Labo");
+        this.primaryStage.show();
+        initRootLayout();
+
+	}
+	
+	 /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(EMR.class.getResource("factoryUI.fxml"));
+            System.out.println(EMR.class.getResource("factoryUI.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
