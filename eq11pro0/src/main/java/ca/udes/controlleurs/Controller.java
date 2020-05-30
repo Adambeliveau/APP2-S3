02638f8591a7cf6c3e9b59378e694e0a7de7b481
AdamBeliveau <bela1003@usherbrooke.ca>
2020-05-30 16:12:15 -0400

    GuI

--- eq11pro0/src/main/java/ca/udes/controlleurs/Controller.java
+++ eq11pro0/src/main/java/ca/udes/controlleurs/Controller.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,6 +32,78 @@ public class Controller {
     private MenuItem FullArrowbtn;
 
     @FXML
+    private Button MultiCoupbtn;
+
+    @FXML
+    private TitledPane EnergyBasedTab;
+
+    @FXML
+    private Button EstimatorMonoCoupbtn;
+
+    @FXML
+    private Button EstimatorCoupInvWeigbtn;
+
+    @FXML
+    private Menu Edit;
+
+    @FXML
+    private TitledPane InversionbasedTab;
+
+    @FXML
+    private Label ModesL;
+
+    @FXML
+    private Button MonoCoupbtn;
+
+    @FXML
+    private Button EstimatorMultiConvbtn;
+
+    @FXML
+    private Button Accumulateurbtn;
+
+    @FXML
+    private Button Expandbtn;
+
+    @FXML
+    private Button Addbtn;
+
+    @FXML
+    private Label AlignementL;
+
+    @FXML
+    private Button TopAlignbtn;
+
+    @FXML
+    private Button Strategybtn;
+
+    @FXML
+    private Canvas Canvas;
+
+    @FXML
+    private Button CouplingInvWeightingbtn;
+
+    @FXML
+    private Button EstimatorIndirectInvbtn;
+
+    @FXML
+    private Button DirectInvDisturbancebtn;
+
+    @FXML
+    private Button Picturebtn;
+
+    @FXML
+    private Button EnergySourcebtn;
+
+    @FXML
+    private Button MonoConvbtn;
+
+    @FXML
+    private Menu File;
+
+    @FXML
+    private Label InversionL;
+
+    @FXML
     private BorderPane BorderPane;
 
     @FXML
@@ -46,43 +119,31 @@ public class Controller {
     private Button Drawbtn;
 
     @FXML
-    private TitledPane EnergyBasedTab;
-
-    @FXML
     private MenuItem SimpleArrowbtn;
-
-    @FXML
-    private Menu Edit;
-
-    @FXML
-    private TitledPane InversionbasedTab;
-
-    @FXML
-    private Label ModesL;
 
     @FXML
     private Tab EditorTab;
 
     @FXML
+    private Button EstimatorCoupInvDistbtn;
+
+    @FXML
     private Button Erasebtn;
+
+    @FXML
+    private Button DirectInvbtn;
 
     @FXML
     private TitledPane StrategybasedTab;
 
     @FXML
-    private Button Expandbtn;
-
-    @FXML
-    private Button Addbtn;
-
-    @FXML
-    private Label AlignementL;
-
-    @FXML
-    private Button TopAlignbtn;
+    private Button EstimatorEnergySourcebtn;
 
     @FXML
     private Tab SimulatorTab;
+
+    @FXML
+    private Button EstimatorMultiCoupbtn;
 
     @FXML
     private Button FullScreenbtn;
@@ -97,22 +158,37 @@ public class Controller {
     private Button LeftAlignbtn;
 
     @FXML
+    private Button EstimatorStrategybtn;
+
+    @FXML
     private MenuBar Menubar;
+
+    @FXML
+    private Button CouplingInvDistributionbtn;
+
+    @FXML
+    private Button EstimatorMonoConvbtn;
+
+    @FXML
+    private Button MultiConvbtn;
 
     @FXML
     private Accordion Accordion;
 
     @FXML
-    private Button Picturebtn;
+    private Button IndirectInvbtn;
 
     @FXML
-    private Menu File;
+    private Button EstimatorDirectInvbtn;
 
     @FXML
-    private Label InversionL;
+    private Button EstimatorAccumulateurbtn;
 
     @FXML
     private Button Inversionbtn;
+
+    @FXML
+    private Button EstimatorDirectInvDisbtn;
 
     @FXML
     void addAction(ActionEvent event) {
@@ -180,37 +256,182 @@ public class Controller {
     }
 
     @FXML
+    void directInvDisturbanceAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void directInvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void indirectInvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void couplingInvWeightingAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void couplingInvDistributionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void strategyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorMultiCoupAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorMonoCoupAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorMultiConvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorMonoConvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorEnergySourceAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorAccumulateurAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorstrategyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorCoupInvDistAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorCoupInvWeigthAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorIndirectInvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorDrectInvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estimatorDirectInvDisAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AccumulateurAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void energySourceAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void monoConvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiConvAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void monoCoupAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiCoupAction(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
         assert MenuArrow != null : "fx:id=\"MenuArrow\" was not injected: check your FXML file 'UI.fxml'.";
         assert FullArrowbtn != null : "fx:id=\"FullArrowbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert MultiCoupbtn != null : "fx:id=\"MultiCoupbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EnergyBasedTab != null : "fx:id=\"EnergyBasedTab\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorMonoCoupbtn != null : "fx:id=\"EstimatorMonoCoupbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorCoupInvWeigbtn != null : "fx:id=\"EstimatorCoupInvWeigbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Edit != null : "fx:id=\"Edit\" was not injected: check your FXML file 'UI.fxml'.";
+        assert InversionbasedTab != null : "fx:id=\"InversionbasedTab\" was not injected: check your FXML file 'UI.fxml'.";
+        assert ModesL != null : "fx:id=\"ModesL\" was not injected: check your FXML file 'UI.fxml'.";
+        assert MonoCoupbtn != null : "fx:id=\"MonoCoupbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorMultiConvbtn != null : "fx:id=\"EstimatorMultiConvbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Accumulateurbtn != null : "fx:id=\"Accumulateurbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Expandbtn != null : "fx:id=\"Expandbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Addbtn != null : "fx:id=\"Addbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert AlignementL != null : "fx:id=\"AlignementL\" was not injected: check your FXML file 'UI.fxml'.";
+        assert TopAlignbtn != null : "fx:id=\"TopAlignbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Strategybtn != null : "fx:id=\"Strategybtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Canvas != null : "fx:id=\"Canvas\" was not injected: check your FXML file 'UI.fxml'.";
+        assert CouplingInvWeightingbtn != null : "fx:id=\"CouplingInvWeightingbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorIndirectInvbtn != null : "fx:id=\"EstimatorIndirectInvbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert DirectInvDisturbancebtn != null : "fx:id=\"DirectInvDisturbancebtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert Picturebtn != null : "fx:id=\"Picturebtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EnergySourcebtn != null : "fx:id=\"EnergySourcebtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert MonoConvbtn != null : "fx:id=\"MonoConvbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert File != null : "fx:id=\"File\" was not injected: check your FXML file 'UI.fxml'.";
+        assert InversionL != null : "fx:id=\"InversionL\" was not injected: check your FXML file 'UI.fxml'.";
         assert BorderPane != null : "fx:id=\"BorderPane\" was not injected: check your FXML file 'UI.fxml'.";
         assert Help != null : "fx:id=\"Help\" was not injected: check your FXML file 'UI.fxml'.";
         assert EstimatorTab != null : "fx:id=\"EstimatorTab\" was not injected: check your FXML file 'UI.fxml'.";
         assert DoubleArrowbtn != null : "fx:id=\"DoubleArrowbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert Drawbtn != null : "fx:id=\"Drawbtn\" was not injected: check your FXML file 'UI.fxml'.";
-        assert EnergyBasedTab != null : "fx:id=\"EnergyBasedTab\" was not injected: check your FXML file 'UI.fxml'.";
         assert SimpleArrowbtn != null : "fx:id=\"SimpleArrowbtn\" was not injected: check your FXML file 'UI.fxml'.";
-        assert Edit != null : "fx:id=\"Edit\" was not injected: check your FXML file 'UI.fxml'.";
-        assert InversionbasedTab != null : "fx:id=\"InversionbasedTab\" was not injected: check your FXML file 'UI.fxml'.";
-        assert ModesL != null : "fx:id=\"ModesL\" was not injected: check your FXML file 'UI.fxml'.";
         assert EditorTab != null : "fx:id=\"EditorTab\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorCoupInvDistbtn != null : "fx:id=\"EstimatorCoupInvDistbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert Erasebtn != null : "fx:id=\"Erasebtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert DirectInvbtn != null : "fx:id=\"DirectInvbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert StrategybasedTab != null : "fx:id=\"StrategybasedTab\" was not injected: check your FXML file 'UI.fxml'.";
-        assert Expandbtn != null : "fx:id=\"Expandbtn\" was not injected: check your FXML file 'UI.fxml'.";
-        assert Addbtn != null : "fx:id=\"Addbtn\" was not injected: check your FXML file 'UI.fxml'.";
-        assert AlignementL != null : "fx:id=\"AlignementL\" was not injected: check your FXML file 'UI.fxml'.";
-        assert TopAlignbtn != null : "fx:id=\"TopAlignbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorEnergySourcebtn != null : "fx:id=\"EstimatorEnergySourcebtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert SimulatorTab != null : "fx:id=\"SimulatorTab\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorMultiCoupbtn != null : "fx:id=\"EstimatorMultiCoupbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert FullScreenbtn != null : "fx:id=\"FullScreenbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert Infobtn != null : "fx:id=\"Infobtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert OptionL != null : "fx:id=\"OptionL\" was not injected: check your FXML file 'UI.fxml'.";
         assert LeftAlignbtn != null : "fx:id=\"LeftAlignbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorStrategybtn != null : "fx:id=\"EstimatorStrategybtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert Menubar != null : "fx:id=\"Menubar\" was not injected: check your FXML file 'UI.fxml'.";
+        assert CouplingInvDistributionbtn != null : "fx:id=\"CouplingInvDistributionbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorMonoConvbtn != null : "fx:id=\"EstimatorMonoConvbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert MultiConvbtn != null : "fx:id=\"MultiConvbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert Accordion != null : "fx:id=\"Accordion\" was not injected: check your FXML file 'UI.fxml'.";
-        assert Picturebtn != null : "fx:id=\"Picturebtn\" was not injected: check your FXML file 'UI.fxml'.";
-        assert File != null : "fx:id=\"File\" was not injected: check your FXML file 'UI.fxml'.";
-        assert InversionL != null : "fx:id=\"InversionL\" was not injected: check your FXML file 'UI.fxml'.";
+        assert IndirectInvbtn != null : "fx:id=\"IndirectInvbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorDirectInvbtn != null : "fx:id=\"EstimatorDirectInvbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorAccumulateurbtn != null : "fx:id=\"EstimatorAccumulateurbtn\" was not injected: check your FXML file 'UI.fxml'.";
         assert Inversionbtn != null : "fx:id=\"Inversionbtn\" was not injected: check your FXML file 'UI.fxml'.";
+        assert EstimatorDirectInvDisbtn != null : "fx:id=\"EstimatorDirectInvDisbtn\" was not injected: check your FXML file 'UI.fxml'.";
 
     }
 }
